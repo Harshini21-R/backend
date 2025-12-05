974a61e2fbe402c3e450437f574aa68533058b1a
Yashwanth Raj KS <yashwanth8v@gmail.com>
2025-12-05 16:37:17 +0530

    Migrate email implementation to Gmail API

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const OAuth2 = google.auth.OAuth2;
 
-const getGmailClient = () => {
+const getTransporter = () => {
     const oauth2Client = new OAuth2(
         process.env.CLIENT_ID,
         process.env.CLIENT_SECRET,
@@ -15,40 +15,33 @@ const getGmailClient = () => {
         refresh_token: process.env.REFRESH_TOKEN
     });
 
-    return google.gmail({ version: 'v1', auth: oauth2Client });
+    return nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: process.env.EMAIL_USER,
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            refreshToken: process.env.REFRESH_TOKEN
+        }
+    });
 };
 
 const sendEmail = async ({ to, subject, text, html }) => {
     try {
-        const gmail = getGmailClient();
+        const transporter = getTransporter();
 
-        // Use Nodemailer to create the raw MIME message
-        const mailComposer = new nodemailer.MailComposer({
+        const mailOptions = {
             from: process.env.EMAIL_USER,
             to,
             subject,
             text,
             html
-        });
+        };
 
-        const message = await mailComposer.compile().build();
-
-        // Encode the message to base64url format required by Gmail API
-        const rawMessage = Buffer.from(message)
-            .toString('base64')
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/=+$/, '');
-
-        const res = await gmail.users.messages.send({
-            userId: 'me',
-            requestBody: {
-                raw: rawMessage
-            }
-        });
-
-        console.log("Email sent via Gmail API: ", res.data);
-        return res.data;
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent via Gmail API:", info.messageId);
+        return info;
     } catch (error) {
         console.error("Error sending email via Gmail API:", error);
         throw error;
